68ef6834918502523dc7056bdca025aba25f70ee
Tae Woo Kim <taewookim02@gmail.com>
2023-02-24 00:20:24 +0800

    Add eventListeners to manipulate DOM

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
 const Gameboard = (() => {
-  const board = ["O", "O", "X", "X", "O", "X", "X", "O", "X"];
+  const _board = new Array(9);
 
-  const DisplayController = () => {
-    const cells = document.querySelectorAll("[data-cell]");
-    cells.forEach((val, i) => {
-      val.textContent = board[i];
+  return { _board };
+})();
+
+const DisplayController = (() => {
+  const _cells = document.querySelectorAll("[data-cell]");
+
+  const _handleCellClick = (e) => {
+    if (e.target.textContent) return;
+    if (main.currentPlayer.mark === "X") {
+      e.target.textContent = "X";
+      return (main.currentPlayer = main.player2);
+    } else {
+      e.target.textContent = "O";
+      return (main.currentPlayer = main.player1);
+    }
+  };
+
+  const loadBoard = () => {
+    _cells.forEach((val, i) => {
+      val.textContent = Gameboard._board[i];
     });
   };
 
-  DisplayController();
-  return { DisplayController };
+  const attachEventListenerToCells = () => {
+    _cells.forEach((val, i) => {
+      val.addEventListener("click", _handleCellClick);
+    });
+  };
+
+  return { loadBoard, attachEventListenerToCells };
 })();
 
 const Players = (name, mark) => {
   return { name, mark };
 };
 
-// Build the functions that allow players to add marks to a specific spot on the board
+const main = (() => {
+  DisplayController.attachEventListenerToCells();
+  const player1 = Players("John Doe", "X");
+  const player2 = Players("Alice Kwon", "O");
+  // player1 goes first
+  let currentPlayer = player1;
 
-// Tie it to the Dom
+  return { currentPlayer, player1, player2 };
+})();
+
+// For the sake of developing the logic and have some progress, I am going to start with X
+
+// Build the functions that allow players to add marks to a specific spot on the board
 
 // Let players click on the gameboard to place marker
 
